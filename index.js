5c81ec884e621fd3bf77731429b9c719d15154ad
anvaka <anvaka@gmail.com>
2014-07-11 21:25:54 -0700

    Fetching rate limit

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-module.exports = require('an').directive( githuboauth, 'githuboauth');
+module.exports = require('an').directive(githuboauth, 'githuboauth');
 var Cookies = require('cookies-js');
 
-function githuboauth() {
+function githuboauth($http) {
+  var rateLimitUnknown = {
+    limit: '?',
+    remaining: '?',
+    reset: '?'
+  };
+
   return {
     restrict: 'ACE',
     replace: true,
     scope: {},
     template: require('fs').readFileSync(__dirname + '/index.html', 'utf8'),
-    link: function ($scope, element, attr) {
-       $scope.isAuthenticated = Cookies.get('accessToken');
-       $scope.clientId = attr.clientId;
-    }
+    link: link
   };
+
+  function link($scope, element, attr) {
+    $scope.isAuthenticated = Cookies.get('accessToken');
+    $scope.clientId = attr.clientId;
+    $scope.rate = rateLimitUnknown;
+    if (!$scope.isAuthenticated) {
+      $http.get('https://api.github.com/rate_limit')
+        .success(function(response, code) {
+          if (code !== 200) return;
+          $scope.rate = response.rate;
+        });
+    }
+  }
 }
+
+githuboauth.$inject = ['$http'];
