a83c98b2da7c1788a50d20521338ceeda6baf95a
anvaka <anvaka@gmail.com>
2014-07-12 20:34:21 -0700

    Added github oauth proxy, a heroku app

--- index.js
+++ index.js
@@ -17,17 +17,53 @@ function githuboauth($http) {
   };
 
   function link($scope, element, attr) {
-    $scope.isAuthenticated = Cookies.get('accessToken');
+    var weGotCodeFromGithub = window.location.href.match(/code=([^&#]*)/);
+    var cookieName = attr.cookiename || 'accessToken';
+
+    $scope.isAuthenticated = Cookies.get(cookieName);
     $scope.clientId = attr.clientid;
     $scope.rate = rateLimitUnknown;
-    if (!$scope.isAuthenticated) {
-      $http.get('https://api.github.com/rate_limit')
+
+    if ($scope.isAuthenticated) {
+      updateUserInfo();
+    } else if (weGotCodeFromGithub) {
+      tradeCodeForToken(attr.oauthproxy, weGotCodeFromGithub[1]);
+    }
+
+    updateRateLimit();
+
+
+    function updateUserInfo() {
+      $http.get('https://api.github.com/user?access_token=' + Cookies.get(cookieName))
+        .success(function(user, code) {
+          if (code !== 200) return;
+          $scope.user = user;
+        });
+    }
+
+    function tradeCodeForToken(oauthProxy, code) {
+      if (!oauthProxy) throw new Error('javascript based oauth is not supported by github. Please setup oauth proxy. See documentation: https://github.com/anvaka/githuboauth');
+
+      var suffix = (oauthProxy[oauthProxy.length - 1] === '/' ? '' : '/') + code;
+      $http.get(oauthProxy + suffix)
+        .success(function(response, code) {
+          if (code !== 200) return;
+          if (response && response.token) {
+            Cookies.set(cookieName, response.token);
+            updateRateLimit();
+          }
+        });
+    }
+
+    function updateRateLimit() {
+      $http.get('https://api.github.com/rate_limit?access_token=' + Cookies.get(cookieName))
         .success(function(response, code) {
           if (code !== 200) return;
           $scope.rate = response.rate;
         });
     }
   }
+
 }
 
 githuboauth.$inject = ['$http'];
